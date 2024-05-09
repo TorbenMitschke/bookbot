7ade98fda43d2f512d51fd54faea00acb4094c70
TorbenMitschke <torben.mitschke@gmail.com>
2024-05-09 21:39:02 +0200

    started function to get sorted list of dictionaries from the char_dict

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 def main():
     book_path = "./books/frankenstein.txt"
     text = get_book_text(book_path)
-    wc = number_of_words(text)
-    print(f"{wc} words were found in the document.")
+    wc = get_number_of_words(text)
+    print(f"{wc} words found in the document.")
     char_dict = get_char_count_dict(text)
     print(char_dict)
+    list_of_dict = get_list_of_dict(char_dict)
+    print(list_of_dict)
 
 def get_book_text(book_path):
     with open(book_path) as f:
         return f.read()
 
-def number_of_words(text):
+def get_number_of_words(text):
     return len(text.split())
 
 def get_char_count_dict(text):
@@ -23,6 +25,12 @@ def get_char_count_dict(text):
             else:
                 letter_count[character] = 1
     return letter_count
+
+def get_list_of_dict(dict):
+    list_of_dict = []
+    for key in dict:
+        list_of_dict.append({key: dict[key]})
+    return list_of_dict
 
 if __name__ == '__main__':
     main()
